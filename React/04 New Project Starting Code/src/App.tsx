974dfc2657ce7b4e6f2cd0d7fc91a1f18ba83f66
ivangguz <ivangguz@gmail.com>
2024-10-15 19:35:52 -0600

    x

---
+++ React/04 New Project Starting Code/src/App.tsx
@@ -0,0 +1,14 @@
+import Button from "./components/Button";
+
+function App() {
+  return <main>
+    <p>
+      <Button>A Button</Button>
+    </p>
+    <p>
+      <Button href="https://www.google.com">A Link</Button>
+    </p>
+  </main>
+}
+
+export default App;
